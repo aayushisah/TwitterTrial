df3c02d9bb2835fc4977799860401dc3c99b7c39
aayushisah <aayushisah313@gmail.com>
2024-04-29 20:29:14 +0530

    Fixed the user feed response to an object instead of a JSON

--- src/main/java/com/example/twittertrial/Controller/UserFeedController.java
+++ src/main/java/com/example/twittertrial/Controller/UserFeedController.java
@@ -1,11 +1,18 @@
 package com.example.twittertrial.Controller;
 
+import com.example.twittertrial.Entity.Comment;
+import com.example.twittertrial.Entity.Post;
+import com.example.twittertrial.Entity.User;
 import com.example.twittertrial.Service.UserFeedService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserFeedController {
@@ -32,4 +39,48 @@ public class UserFeedController {
 //                    .body("An error occurred while fetching user feed data");
 //        }
 //    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<?> getUserFeed() {
+        List<Post> posts = userFeedService.getAllPosts();
+
+        // Create a JSON array to represent all posts
+        JSONArray postsArray = new JSONArray();
+        for (Post post : posts) {
+            // Create a new JSON object to represent the post details
+            JSONObject postObject = new JSONObject();
+            postObject.put("postID", post.getID());
+            postObject.put("postBody", post.getPostBody());
+            postObject.put("date", post.getDate());
+
+            // Create a JSON array to represent comments
+            JSONArray commentsArray = new JSONArray();
+            for (Comment comment : post.getComments()) {
+                JSONObject commentObject = new JSONObject();
+                commentObject.put("commentID", comment.getID());
+                commentObject.put("commentBody", comment.getCommentBody());
+
+                // Retrieve user details and set name
+                User user = comment.getUser();
+                if (user != null) {
+                    JSONObject commentCreator = new JSONObject();
+                    commentCreator.put("userID", user.getID());
+                    commentCreator.put("name", user.getName());
+                    commentObject.put("commentCreator", commentCreator);
+                } else {
+                    commentObject.put("commentCreator", null);
+                }
+
+                commentsArray.add(commentObject);
+            }
+            postObject.put("comments", commentsArray);
+
+            // Add the post object to the posts array
+            postsArray.add(postObject);
+        }
+
+        // Return the constructed JSON array as the response
+        return ResponseEntity.ok(postsArray);
+    }
 }
--- src/main/java/com/example/twittertrial/Service/UserFeedService.java
+++ src/main/java/com/example/twittertrial/Service/UserFeedService.java
@@ -1,11 +1,13 @@
 package com.example.twittertrial.Service;
 
+import com.example.twittertrial.Entity.Post;
 import com.example.twittertrial.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserFeedService {
@@ -24,5 +26,13 @@ public class UserFeedService {
         // Additional processing or filtering of user feed data can be done here
 
         return userFeedData.get(0);
+    }
+
+    public List<Post> getAllPosts() {
+        // Fetch all posts from the repository and sort them in reverse chronological order
+        List<Post> posts = postRepository.findAll();
+        return posts.stream()
+                .sorted((post1, post2) -> post2.getDate().compareTo(post1.getDate()))
+                .collect(Collectors.toList());
     }
 }
